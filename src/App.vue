359f01546ae584a4b76c9feab91aaf39725628a7
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-21 11:04:42 +0100

    Finished the project

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import { ref } from 'vue';
 
 const bands = ref(['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']);
 
+const removeArticles = str => {
+    return str.replace(/^(a |the |an )/i, '').trim();
+}
+
+bands.value = bands.value.sort( (a, b) => removeArticles(a) > removeArticles(b)? 1 : -1);
 </script>
 
 <template>
